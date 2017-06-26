15e0d58184d73d04f63f1fc5c2df48068c989890
shirlaine <shirlaineph@gmail.com>
2017-06-26 17:53:02 +0800

    Change video path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 	resources :posts
 	get 'welcome/index'
 	root 'welcome#index'
-	get 'welcome/video'
+	get 'welcome/video' => 'welcome#video'
 	#root "posts#homepage"
 	#root "posts#index"
 	
